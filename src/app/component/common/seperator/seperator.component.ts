7d9c8a045123b0aee62a9e272a7c6d70446411c1
quh <quhxuxm_quh@163.com>
2018-11-22 15:34:31 +0800

    init

--- src/app/component/common/seperator/seperator.component.ts
+++ src/app/component/common/seperator/seperator.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'nwen-common-seperator',
+  selector: 'nwen-seperator',
   templateUrl: './seperator.component.html',
   styleUrls: ['./seperator.component.scss']
 })
 export class SeperatorComponent implements OnInit {
+  @Input()
+  showLeftBorder: boolean;
+  @Input()
+  showRightBorder: boolean;
+  @Input()
+  showContent: boolean;
 
-  constructor() { }
+  constructor() {
+    this.showLeftBorder = true;
+    this.showRightBorder = true;
+    this.showContent = false;
+  }
 
   ngOnInit() {
   }
-
 }
